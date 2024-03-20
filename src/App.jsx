9d013b6ea76f8bcdfc340f21176ee667370d6729
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-03-20 09:08:37 +0545

    create a roadmap components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 
@@ -17,7 +18,7 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
-        {/* <Roadmap /> */}
+        <Roadmap />
         {/* <Footer /> */}
       </div>
 
