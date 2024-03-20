53f825e44bb463bf984c71d84138965bb03d4a65
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-03-20 08:52:56 +0545

    create a service components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
-        {/* <Services /> */}
+        <Services />
         {/* <Pricing /> */}
         {/* <Roadmap /> */}
         {/* <Footer /> */}
