a8368ea2349d6ee25028eb38a73e3c7e2741bc26
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-03-20 08:20:14 +0545

    make the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 
 const App = () => {
@@ -7,13 +8,13 @@ const App = () => {
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        {/* <Hero />
-        <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
-        <Footer /> */}
+        <Hero />
+        {/* <Benefits /> */}
+        {/* <Collaboration /> */}
+        {/* <Services /> */}
+        {/* <Pricing /> */}
+        {/* <Roadmap /> */}
+        {/* <Footer /> */}
       </div>
 
       <ButtonGradient />
