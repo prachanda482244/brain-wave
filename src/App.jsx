273f4867c246eba7f2ca9d48415f24deaefcd740
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-03-20 08:34:45 +0545

    minor change to benefits components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
